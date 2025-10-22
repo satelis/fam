64e882b9926588ee14ee2af8b0f5e055095f33bb
Henrique Satelis <henriquefsatelis@gmail.com>
2025-10-21 21:19:28 -0300

    fixed visual studio project

---
+++ est-dados-n1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace est_dados_n1 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<Registro> registroList = new List<Registro>();
+        }
+    }
+}
+
