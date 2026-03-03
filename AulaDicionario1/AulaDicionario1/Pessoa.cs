bd4815bc34fcfbd344604e108651cb01debf8f94
hiq <118542728+satelis@users.noreply.github.com>
2026-03-03 20:22:17 -0300

    feat dictionary lesson
    added old lesson that wasnt on repo

---
+++ AulaDicionario1/AulaDicionario1/Pessoa.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaDicionario1 {
+    internal class Pessoa {
+        private string nome;
+        private string cidade;
+        private double renda;
+        private int filhos;
+        private bool atividade;
+        private char genero;
+        private int tempoEmpresa;
+        private bool promovido;
+        private Dictionary<int, Pessoa> dp;
+
+        public Pessoa(string nome, string cidade, double renda, int filhos, bool atividade, char genero, int tempoEmpresa, bool promovido) {
+            this.nome = nome;
+            this.cidade = cidade;
+            this.renda = renda;
+            this.filhos = filhos;
+            this.atividade = atividade;
+            this.genero = genero;
+            this.tempoEmpresa = tempoEmpresa;
+            this.promovido = promovido;
+        }
+        
+        public Pessoa(Dictionary<int, Pessoa> p) {
+            this.dp = p;
+        }
+
+        public void ImprimeChaves(Dictionary<int, Pessoa> pes) {
+            foreach (var item in pes.Keys) {
+                Console.WriteLine("{0}", item);
+            }
+        }
+
+        public void ImprimeValores(Dictionary<int, Pessoa> pes) {
+            Console.WriteLine("\nImpressão dos valores:");
+            foreach (var item in pes.Values) {
+                Console.WriteLine("{0, -20} {1, -22} {2, 12:C2} {3, 6} {4, 10} {5, 8} {6, 4} {7, 9}", item.nome, item.cidade, item.renda, item.filhos, item.atividade, item.genero, item.tempoEmpresa, item.promovido);
+            }
+        }
+
+        public void ImprimeDicionario() {
+            Console.WriteLine("\nImprimindo dicionário:");
+            foreach (var item in dp) {
+                Console.WriteLine("{0, 4} {1, -21} {2, -22} {3, 12:C2} {4, 6} {5, 10} {6, 8} {7, 4} {8, 9}", item.Key, item.Value.nome, item.Value.cidade, item.Value.renda, item.Value.filhos, item.Value.atividade, item.Value.genero, item.Value.tempoEmpresa, item.Value.promovido);
+
+            }
+        }
+    } 
+}
---
+++ AulaDicionario1/AulaDicionario1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaDicionario1 {
+    internal class Program {
+        static void Main(string[] args) {
+            Dictionary<int, Pessoa> dic = new Dictionary<int, Pessoa>() {
+                {1, new Pessoa("Zé","Americana", 4321.10, 2, true, 'M', 3, false) },
+                {2, new Pessoa("Patasquio Pataqua", "Limeira", 3698.24, 2, false, 'M', 3, false) },
+                {3, new Pessoa("Tiburcio Coiote", "São Paulo", 3723.60, 3, true, 'M', 1, true)},
+                {4, new Pessoa("Rozalino Ritalino", "São Paulo", 4501.37, 1, true, 'M', 4, true)},
+                {5, new Pessoa("Faganeto Rogueti", "Santa Bárbara d´Oeste", 2992.05, 2, true, 'M', 3, true)},
+                {6, new Pessoa("Astrogildo Samanco", "Sumaré", 2850.48, 0, true, 'M', 1, true)},
+                {7, new Pessoa("Chica da Silva", "Rio Claro", 3481.57, 0, true, 'F', 1, true)},
+                {8, new Pessoa("Juvenal Mosquera", "São Paulo", 3028.51, 0, true, 'M', 0, false)},
+                {9, new Pessoa("Margarida Viola", "Piracicaba", 3569.92, 2, true, 'F', 3, true)},
+                {10, new Pessoa("Dafra Topata", "Piracicaba", 1987.58, 1, false, 'F', 4, true)},
+                {11, new Pessoa("Raquelina Albra", "Santa Bárbara d´Oeste", 3718.94, 2, true, 'F', 4, false)},
+                {12, new Pessoa("Tormentina Mianto", "Sumaré", 2845.48, 1, true, 'F', 4, true)},
+                {13, new Pessoa("Fizema Igarro", "Americana", 3937.45, 2, true, 'F', 2, false)},
+                {14, new Pessoa("Bartina Lagame", "Piracicaba", 4074.37, 3, true, 'F', 3, true)},
+                {15, new Pessoa("Cremilda Tutibona", "Americana", 4873.04, 1, true, 'F', 2, false)},
+                {16, new Pessoa("Pragilda Lunataque", "Limeira", 3985.82, 1, true, 'F', 4, false)},
+                {17, new Pessoa("Piangela Alvira", "São Paulo", 4034.95, 4, true, 'F', 4, true)},
+                {18, new Pessoa("Gatunilda Amiuna", "Santa Bárbara d´Oeste", 3639.47, 2, true, 'F', 4, false)},
+                {19, new Pessoa("Lartina Dumante", "Limeira", 2957.84, 0, true, 'F', 5, true)},
+                {20, new Pessoa("Ursula Cavasca", "Americana", 3748.48, 2, true, 'F', 4, false)},
+                {21, new Pessoa("Alairton Masquero", "Piracicaba", 3981.42, 2, true, 'M', 5, false)}
+            };
+
+            Pessoa p = new Pessoa(dic);
+            p.ImprimeChaves(dic);
+            p.ImprimeValores(dic);
+            p.ImprimeDicionario();
+
+            /* PRIMEIROS EXEMPLOS
+            Dictionary<int, string> maquina = CarregaDados();
+            ImprimeChaves(maquina);
+            ImprimeValores(maquina);
+            ImprimeDicionario(maquina);
+            */
+        }
+
+        static Dictionary<int, string> CarregaDados() {
+        Dictionary<int, string> carro = new Dictionary<int, string>() {
+            {1, "Marea"},
+            {2, "Eclipse"},
+            {3, "Caravan"},
+            {4, "Opala"}
+        };
+
+        return carro;   
+        }
+
+        static void ImprimeChaves(Dictionary<int, string> car) {
+            Console.WriteLine("1ª forma");
+            foreach (var item in car) {
+                Console.WriteLine("{0}", item.Key);
+            }
+
+            Console.WriteLine("\n2ª forma");
+            foreach (var item in car.Keys) {
+                Console.WriteLine("{0}", item);
+            } 
+        }
+
+        static void ImprimeValores(Dictionary<int, string> car) {
+            Console.WriteLine("1ª forma");
+            foreach (var item in car) {
+                Console.WriteLine("{0}", item.Value);
+            }
+
+            Console.WriteLine("\n2ª forma");
+            foreach (var item in car.Values) {
+                Console.WriteLine("{0}", item);
+            } 
+        }
+        static void ImprimeDicionario(Dictionary<int, string> car) {
+            Console.WriteLine("\nImprimindo o dicionário");
+            foreach (var item in car) {
+                Console.WriteLine("{0} {1}", item.Key, item.Value);
+            }
+        }
+
+    }
+}
